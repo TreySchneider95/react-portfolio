538bf0f9edd971295b53c4f036da88b5cbfffcde
TreySchneider95 <tschneid95@gmail.com>
2023-10-10 19:50:30 -0700

    changed about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -77,17 +77,42 @@ class About extends React.Component {
         {
           id: "first-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            "My experience in developing web applications spans various technologies, including Python, Django, Flask, React, and Express. I have a proven track record of successfully designing, building, and deploying web solutions that meet clients' needs and drive business growth. My proficiency in both SQL and NoSQL databases allows me to create efficient and scalable database structures, ensuring optimal performance for web applications."
         },
         {
           id: "second-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            "Some highlights of my qualifications include:"
         },
         {
           id: "third-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            "Python Django Expertise: I have worked extensively with Django to create robust and secure web applications. My experience includes designing RESTful APIs, implementing authentication systems, and optimizing database queries for improved performance."
+        },
+        {
+          id: "fourth-p-about",
+          content:
+            "Front-End Development: I have hands-on experience with JavaScript, React, and other front-end technologies, allowing me to create responsive and user-friendly interfaces that enhance the user experience."
+        },
+        {
+          id: "fith-p-about",
+          content:
+            "Back-End Development: My skills in Flask and Express enable me to develop server-side logic and APIs that seamlessly interact with the front-end, ensuring a smooth end-to-end user experience."
+        },
+        {
+          id: "sixth-p-about",
+          content:
+            "Database Management: I am well-versed in both SQL and NoSQL databases, enabling me to choose the right database technology for each project and design efficient data models."
+        },
+        {
+          id: "seventh-p-about",
+          content:
+            "In addition to my technical skills, my background in the Navy has instilled in me a strong sense of teamwork and collaboration. During my service, I had the privilege of working within diverse teams where effective communication, problem-solving, and adaptability were paramount. These experiences have equipped me with the ability to collaborate effectively with cross-functional teams, even in high-pressure environments. Moreover, my time as a controls engineer honed my ability to work independently and take initiative, ensuring that I can tackle challenges autonomously and deliver results in a timely manner."
+        },
+        {
+          id: "eighth-p-about",
+          content:
+            "I am genuinely passionate about web development and staying up-to-date with the latest industry trends and best practices. My ability to quickly adapt to new technologies and my commitment to writing clean, maintainable code have been key factors in my professional success."
         }
       ]
     };
